7c067967b6993fa4b764a57d645dfcd08b04864a
Jeroen Cappaert <3653455+jcappaert@users.noreply.github.com>
2026-06-02 17:11:50 +0200

    Add silent mode switch (#42)

--- components/daikin_ekhhe/switch.py
+++ components/daikin_ekhhe/switch.py
@@ -1,10 +1,14 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import switch
-from esphome.const import CONF_ID, ENTITY_CATEGORY_DIAGNOSTIC
+from esphome.const import ENTITY_CATEGORY_CONFIG, ENTITY_CATEGORY_DIAGNOSTIC
 
 from . import CONF_EKHHE_ID, DaikinEkhhe, daikin_ekhhe_ns
-from .const import DAIKIN_DEBUG_FREEZE
+from .const import DAIKIN_DEBUG_FREEZE, SILENT_MODE
+
+DaikinEkhheSwitch = daikin_ekhhe_ns.class_(
+    "DaikinEkhheSwitch", switch.Switch, cg.Component
+)
 
 DaikinEkhheDebugSwitch = daikin_ekhhe_ns.class_(
     "DaikinEkhheDebugSwitch", switch.Switch, cg.Component
@@ -13,6 +17,9 @@ DaikinEkhheDebugSwitch = daikin_ekhhe_ns.class_(
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(CONF_EKHHE_ID): cv.use_id(DaikinEkhhe),
+        cv.Optional(SILENT_MODE): switch.switch_schema(
+            DaikinEkhheSwitch, entity_category=ENTITY_CATEGORY_CONFIG
+        ),
         cv.Optional(DAIKIN_DEBUG_FREEZE): switch.switch_schema(
             DaikinEkhheDebugSwitch, entity_category=ENTITY_CATEGORY_DIAGNOSTIC
         ),
@@ -22,6 +29,12 @@ CONFIG_SCHEMA = cv.Schema(
 
 async def to_code(config):
     hub = await cg.get_variable(config[CONF_EKHHE_ID])
+    if SILENT_MODE in config:
+        conf = config[SILENT_MODE]
+        sw = await switch.new_switch(conf)
+        cg.add(hub.register_switch(SILENT_MODE, sw))
+        cg.add(sw.set_parent(hub))
+        cg.add(sw.set_internal_id(SILENT_MODE))
     if DAIKIN_DEBUG_FREEZE in config:
         conf = config[DAIKIN_DEBUG_FREEZE]
         sw = await switch.new_switch(conf)
